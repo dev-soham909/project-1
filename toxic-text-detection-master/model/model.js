7c9018238e770a9917fb57147f641dc7a46dcbb2
dev-soham909 <sohamps3s@gmail.com>
2025-02-03 20:55:27 +0530

    Add files via upload

---
+++ toxic-text-detection-master/model/model.js
@@ -0,0 +1,54 @@
+
+import * as DICTIONARY from './dictionary.js';
+const MODEL_JSON_URL = './model/model.json';
+
+const ENCODING_LENGTH = 21;
+
+var model = undefined;
+
+
+window.loadAndPredict = async function loadAndPredict(inputText) {
+  
+  if (model === undefined) {
+    model = await tf.loadLayersModel(MODEL_JSON_URL);
+  }
+  
+  
+  let lowercaseSentenceArray = inputText.toLowerCase().replace(/[^\w\s]/g, ' ').split(' ');
+  
+  lowercaseSentenceArray = lowercaseSentenceArray.slice(0, 20);
+  
+  
+  let results = model.predict(tokenize(lowercaseSentenceArray));
+  
+ 
+  let dataArray = results.dataSync();
+
+  
+  let toxicityLevel = dataArray[1] * 100;
+
+  
+  console.log('Toxiticy level: ' + toxicityLevel);
+
+  return toxicityLevel;
+}
+
+
+function tokenize(wordArray) {
+  
+  let returnArray = [DICTIONARY.START];
+  
+  
+  for (var i = 0; i < wordArray.length; i++) {
+    let encoding = DICTIONARY.LOOKUP[wordArray[i]];
+    returnArray.push(encoding === undefined ? DICTIONARY.UNKNOWN : encoding);
+  }
+  
+  
+  while (returnArray.length < ENCODING_LENGTH) {
+    returnArray.push(DICTIONARY.PAD);
+  }
+  
+
+  return tf.tensor2d([returnArray]);
+}
